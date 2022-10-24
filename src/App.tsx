01e606bd4a2d7b16cf29f8e6b1b79827115883fa
이재웅 <ijw9209@naver.com>
2022-10-25 02:33:13 +0900

    add Todo complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState } from 'react';
+import { AppDispatch, RootState } from './app/store'
+import { useSelector, useDispatch } from 'react-redux'
+import { addTodo } from './features/todos/todoSlice';
+
 
 function App() {
+  const todoList = useSelector((state: RootState) => state.todos)
+  const dispatch = useDispatch<AppDispatch>()
+
+  console.log(dispatch)
+
+  let [ txt , setTxt ] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>할일 관리</h1>
+
+      <div>
+        <input 
+        type="text"
+        onChange={(e) => setTxt(e.target.value)}
+        ></input>
+        <button
+          onClick={() => {
+            dispatch(addTodo(txt));
+          }}
+        >추가</button>
+      </div>
+
+      <div>
+        <h3>할일 내용</h3>
+        <>
+        {todoList.map((item) =>(
+          <p key={item.id}>{item.text}</p>
+        ))}          
+        </>        
+
+        
+
+      </div>
+    </>
   );
 }
 
---
+++ src/features/todos/todoSlice.ts
@@ -0,0 +1,45 @@
+import { createSlice , PayloadAction } from '@reduxjs/toolkit'
+import type { RootState } from '../../app/store'
+import { v4 as uuidv4 } from 'uuid';
+
+// Define a type for the slice state
+interface Todo {
+  id : string;
+  text : string;
+  completed : boolean
+}
+
+// Define the initial state using that type
+const initialState: Todo[] = [
+    {
+        id : uuidv4(),
+        text : '테스트',
+        completed : false
+    }
+];
+
+export const todoSlice = createSlice({
+  name: 'todos',
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    addTodo: (state , action: PayloadAction<string>) => {
+        // const { text } = action.payload;
+        state.push({id : uuidv4(), text : action.payload, completed : false });
+    },
+    // decrement: (state) => {
+    //   state.value -= 1
+    // },
+    // // Use the PayloadAction type to declare the contents of `action.payload`
+    // incrementByAmount: (state, action: PayloadAction<number>) => {
+    //   state.value += action.payload
+    // },
+  },
+})
+
+export const { addTodo } = todoSlice.actions
+
+// Other code such as selectors can use the imported `RootState` type
+export const selectTodos = (state: RootState) => state.todos
+
+export default todoSlice.reducer
