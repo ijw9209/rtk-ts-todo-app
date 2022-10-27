6073fe72f067df87fdc84913c31d840dd7fde8ee
이재웅 <ijw9209@naver.com>
2022-10-28 01:58:41 +0900

    component분리

---
+++ src/features/todos/TodoItem.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+import { AppDispatch } from "../../app/store";
+import { useDispatch } from "react-redux";
+
+import { deleteTodo } from "./todoSlice";
+
+import { ListItem } from "@mui/material";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+
+
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+
+import styles from './TodoItem.module.css';
+
+interface Todo {
+    id : string;
+    text : string;
+    completed : boolean
+}
+function TodoItem({todo } : {todo: Todo}) {
+
+  const dispatch = useDispatch<AppDispatch>();
+  return (
+    <>
+        <Card className={styles.card} style={{ marginTop: "20px" }}>
+          <CardContent>
+            <ListItem
+              style={{ justifyContent: "space-between" }}
+              disableGutters
+              key={todo.id}
+              // dispatch 추가
+              onClick={() => {
+                dispatch(deleteTodo(todo.id));
+              }}
+            >
+              {todo.text}{" "}
+              <DeleteOutlineIcon
+                style={{ float: "right", cursor: "pointer" }}
+              />
+            </ListItem>
+          </CardContent>
+        </Card>
+    </>
+  );
+}
+
+export default TodoItem;
---
+++ src/features/todos/TodoList.tsx
@@ -0,0 +1,80 @@
+import React, {useState} from 'react'
+
+import { AppDispatch, RootState } from "../../app/store";
+import { useSelector, useDispatch } from "react-redux";
+
+import { addTodo, deleteTodo } from "./todoSlice";
+
+//meterial Ui
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+import List from "@mui/material/List";
+
+//css modules import
+import styles from './TodoList.module.css';
+
+//todoItem 컴포넌트 추가
+import TodoItem from './TodoItem'
+
+function TodoList() {
+  const todoList = useSelector((state: RootState) => state.todos);
+  const dispatch = useDispatch<AppDispatch>();
+
+  // console.log(dispatch);
+
+  let [txt, setTxt] = useState("");
+  return (
+    <CssBaseline>
+      <Container fixed>
+        <Box sx={{ height: "100vh" }}>
+          <h1 className={styles.center}>Todo List</h1>
+          <div className={styles.center}>
+            <TextField
+              type="text"
+              onChange={(e) => setTxt(e.target.value)}
+              id="standard-basic"
+              label="todo Input"
+              variant="standard"
+              value={txt}
+            ></TextField>
+            <Button
+              style={{ marginTop: "10px", marginLeft: "10px" }}
+              variant="outlined"
+              onClick={(e) => {
+                dispatch(addTodo(txt));
+                setTxt('')
+                console.log(e);
+              }}
+            >
+              추가
+            </Button>
+          </div>
+          <div className={styles.center}>
+            <h3>할일 내용</h3>
+            <List
+              dense
+              sx={{
+                margin: "0 auto",
+                width: "100%",
+                maxWidth: 360,
+                bgcolor: "background.paper",
+              }}
+            >
+              {todoList.map((item) => (
+                <TodoItem todo={item}/>
+              ))}
+            </List>
+          </div>
+        </Box>
+      </Container>
+    </CssBaseline>
+  )
+}
+
+
+export default TodoList;
